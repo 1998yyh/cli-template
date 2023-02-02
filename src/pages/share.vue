497bbc68c48e68c7983c3f380801996e61b66e1c
hao.yang <hao.yang@haoyangdeMacBook-Pro.local>
2023-02-02 11:13:33 +0800

    提交

--- src/pages/share.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  share
-</template>
-
-<script setup lang="ts">
-
-</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const three = () => import('../pages/three/index.vue')
 const home = () => import('../pages/home/index.vue')
 
 const routes = [
   { path: '/', redirect: '/home' },
   {
-    path: '/three',
-    name: 'three',
-    component: three
-  }, {
     path: '/home',
     name: 'home',
     component: home
